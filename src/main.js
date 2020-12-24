6d4a68be659514683b9eed4ff546aa08f711f181
botsula@equalteam.org.ua <botsula@equalteam.org.ua>
2020-12-24 10:55:42 +0200

    update

--- src/main.js
+++ src/main.js
@@ -12,18 +12,30 @@ const app = new Vue({
     data: {
         loading: false,
         user: null,
+        lms_raiting: 0,
+        lms_activity: {count: 0, learning: 0},
+        progress: 0,
+        urlHome: null,
         levels: {
             '0': {
-                name: 'Zero',
+                val:0,
+                name: 'Beginner',
                 maxRating: 100
             },
             '1': {
-                name: 'One',
+                val:1,
+                name: 'Beginner',
                 maxRating: 1000
             },
             '2': {
+                val: 2,
                 name: 'Master',
                 maxRating: 5000
+            },
+            '3': {
+                val: 3,
+                name: 'Guru',
+                maxRating: 15000
             }
         },
     },
@@ -44,6 +56,7 @@ const app = new Vue({
                 app.user.level = app.levels[app.user.user_field2] || {};
                 const lvlNames = Object.keys(app.levels);
                 let fl = false;
+                app.user.nextLevel = null;
                 for (const lvlName of lvlNames) {
                     if (fl) {
                         app.user.nextLevel = app.levels[lvlName];
@@ -51,11 +64,38 @@ const app = new Vue({
                     }
                     if (lvlName == app.user.user_field2) {
                         fl = true;
+                        app.progress = app.user.user_field1 * 100 / app.levels[lvlName].maxRating;
+                    }
+                    if (app.user.photo_thumb) {
+                        app.user.photo_thumb = apiServer + app.user.photo_thumb;
                     }
                 }
-                app.user.nextLevel = null;
+                //app.user.nextLevel = null;
                 app.loading = false;
+                
+            })
+            .then(function (response) {
+                console.log(app.user.id)
+                axios
+                .get(`${apiServer}/api/rest.php/auth/users/` + app.user.id + `?action=get-user-activity-data`)
+                .then(function (response) {
+                    let res = response.data;
+                    app.lms_activity.count = res.count;
+                    for (const key in res.data) {
+                        app.lms_activity.learning +=  (res.data[key]) ? res.data[key] : 0;
+                    }
+                });
             });
+
+
+            axios
+            .get(`${apiServer}/api/v2/users/rating/current/value`)
+            .then(function (response) {
+                app.urlHome = apiServer;
+                app.lms_raiting = response.data.rating;
+            });
+
+            
     },
     methods: {}
 });
